69d5f8ae0830bf677ec269a6282f0b90d751973e
Ahmed Jamal <gongool@hotmail.com>
2024-08-22 00:06:18 +0400

    finished i18n app

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Locale } from '@/i18n.config'
+import { getDictionary } from '@/lib/dictionary'
 
-export const page = () => {
+export default async function About({
+  params: { lang }
+}: {
+  params: { lang: Locale }
+}) {
+  const { page } = await getDictionary(lang)
+
   return (
-    <div>page</div>
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>{page.about.title}</h1>
+        <p className='text-gray-500'>{page.about.description}</p>
+      </div>
+    </section>
   )
 }
